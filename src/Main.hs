586e6cbd783088ffd371bbd01691c61fac37f81d
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2025-01-21 14:01:45 +0100

    is this the end, or bug ahead?

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 import Control.Monad (forM_)
 import Data.ByteString.Char8 (pack)
@@ -28,7 +29,9 @@ import Text.Pandoc.Highlighting (pygments, styleToCss, zenburn)
 import Text.Pandoc.Options
 import Text.Pandoc.Walk (walkM)
 
+import GHC.Generics(Generic)
 import System.Environment.Blank (getEnv)
+import System.Posix.Files (fileExist)
 import Debug.Trace
 import Text.Blaze.Html.Renderer.String (renderHtml)
 import Data.Map (Map)
@@ -43,9 +46,13 @@ import Data.Aeson.KeyMap qualified as JK
 import Control.Lens ((^.))
 import Data.Vector (Vector, fromList)
 import Data.Vector qualified as V
+import Data.Vector.Algorithms qualified as VAlg
 import Data.Text (Text)
 import Data.Text qualified as T
 import GHC.IO.Encoding qualified as E
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import Data.Ord qualified as Ord
 
 host :: String
 -- host = "https://jezenthomas.com"
@@ -86,19 +93,29 @@ data StoredMentions = SM {
     likes :: Map WMPost (Vector Value),
     replies :: Map WMPost (Vector Value),
     reposts :: Map WMPost (Vector Value)
-} deriving (Show)
+} deriving (Show, Generic)
 
-instance FromJSON StoredMentions where
+instance ToJSON StoredMentions where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON StoredMentions
+
+emptySM :: StoredMentions
+emptySM = SM mempty mempty mempty
+
+newtype GetWM = GetWM { unGetWM :: StoredMentions } deriving (Show)
+
+instance FromJSON GetWM where
     parseJSON = withObject "webmention" $ \v -> do
         cs <- v .: "children"
         case cs of
-            Array vec -> pure $ SM (foldToMap likes) (foldToMap replies) (foldToMap reposts) where
+            Array vec -> pure . GetWM $ SM (foldToMap likes) (foldToMap replies) (foldToMap reposts) where
 
                 foldToMap objs = Map.fromList $ foldl go mempty (V.groupBy gb objs) where
                     -- go :: [(WMPost, Vector Value)] -> Vector Value -> [(WMPost, Vector Value)]
                     go wmmap ls
                         | V.null ls = wmmap
-                        | otherwise = case traceShow "LOOK: this is head" $ V.head ls of
+                        | otherwise = case V.head ls of
                             Object o -> case JK.lookup "wm-target" o of
                                 Just (String t) -> (drop 8 $ T.unpack t, ls) : wmmap
                                 _ -> wmmap
@@ -158,19 +175,21 @@ main = do
 
 hmain :: IO ()
 hmain = hakyll $ do
-  mentions <- preprocess $ do
+
+
+  newMentions <- preprocess $ do
     webementionIoToken <- getEnv "WMToken"
     case webementionIoToken of
       Nothing -> do
         putStrLn $ "Warning: no webmention.io token found"
         putStrLn $ "Warning: please set WMToken environmental variable"
-        pure $ SM mempty mempty mempty
+        pure emptySM
       Just token -> do
 
         response <- W.get $ "https://webmention.io/api/mentions.jf2?domain=maxfii.github.io&token=" <> token
 
         case response ^. W.responseStatus . W.statusCode of
-          200 -> case eitherDecode $ response ^. W.responseBody of
+          200 -> case fmap unGetWM . eitherDecode $ response ^. W.responseBody of
               Right sm@(SM _ _ _) -> pure sm
               Left err -> do
                 putStrLn $ "Error: decoding webmentions. aeson error: "
@@ -179,7 +198,33 @@ hmain = hakyll $ do
 
           bad -> do
             putStrLn $ "Error: fetching webmentions. status code: " <> show bad
-            pure $ SM mempty mempty mempty
+            pure emptySM
+
+  mentions <- preprocess $ fileExist "webmentions.json" >>= \case
+    True -> do
+      SM oldLikes oldReplies oldReposts <- fromMaybe emptySM . decodeStrict <$> B.readFile "webmentions.json"
+
+      let SM newLikes newReplies newReposts = newMentions
+          result = SM (merge newLikes oldLikes) (merge newReplies oldReplies)
+            (merge oldReposts oldReposts)
+
+          merge = Map.unionWith checkIds
+
+          checkIds :: Vector Value -> Vector Value -> Vector Value
+          checkIds vec1 vec2 = VAlg.nubBy comparison (vec1 <> vec2)
+
+          comparison v1@(Object o1) v2@(Object o2) =
+            fromMaybe (Ord.compare (encode v1) (encode v2)) $
+                Ord.compare <$> JK.lookup "wm-id" o1 <*> JK.lookup "wm-id" o2
+
+          comparison o1 o2 = (Ord.compare (encode o1) (encode o2))
+
+      encodeFile "webmentions.json" result
+      pure result
+
+    False -> do
+      encodeFile "webmentions.json" newMentions
+      pure newMentions
 
   compiledStylesheetPath <- preprocess $ do
     styles <- mapM readFile styleSheets
@@ -222,20 +267,31 @@ hmain = hakyll $ do
     rulesExtraDependencies [dep] $ compile $ do
       ident <- getUnderlying
 
-      let wmreplies = listField "replies" (field "html" (return . itemBody))
+      let wmreplies = listField "replies" (field "repl" (return . itemBody))
             (traverse (makeItem . fromMaybe "" . renderReply)
-                (V.toList . fromMaybe V.empty . Map.lookup target $ likes mentions))
+                (V.toList . fromMaybe V.empty . Map.lookup target $ replies mentions))
 
-          wmlikes = listField "likes" (field "html" (return . itemBody))
+          wmlikes = listField "likes" (field "like" (return . itemBody))
             (traverse (makeItem . fromMaybe "" . renderLike)
                 (V.toList . fromMaybe V.empty . Map.lookup target $ likes mentions))
+
+          wmreposts = listField "reposts" (field "repo" (return . itemBody))
+            (traverse (makeItem . fromMaybe "" . renderRepost)
+                (V.toList . fromMaybe V.empty . Map.lookup target $ reposts mentions))
 
           ctx = postCtx <> utcCtx <> wmreplies <> wmlikes
           cleanTarget = dropExtension . joinPath . drop 1 . splitPath . toFilePath
           target = drop 8 host </> cleanTarget ident <> "/"
 
-      debugCompiler $ "LOOK: reading mentions: " <> (show . Map.keys $ likes mentions)
-      debugCompiler $ "LOOK: reading mentions: " <> (show . Map.keys  $ replies mentions)
+          count = fmap $ foldl (\acc _ -> acc+1) 0
+
+      -- debugCompiler $ "LOOK: reading mentions: " <> (show . Map.lookup "maxfii.github.io/2023/11/stubbing-io-in-yesod/" $ replies mentions)
+      if target == "maxfii.github.io/2023/11/stubbing-io-in-yesod/"
+        then do
+            debugCompiler $ "LOOK: counting replies: " <> (show . count . Map.lookup target $ replies mentions)
+            debugCompiler $ "LOOK: counting likes: " <> (show . count . Map.lookup target $ likes mentions)
+            debugCompiler $ "LOOK: counting reposts: " <> (show . count . Map.lookup target $ reposts mentions)
+        else pure ()
       debugCompiler $ "LOOK: reading target: " <> target
 
 
